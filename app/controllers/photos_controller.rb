3999d42b87c2a9bd78881c2562c2249f3aaae2a4
brdavis330 _ <brandimarie4259@gmail.com>
2023-05-01 20:15:18 +0000

    complete sign in & sign up finished

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,11 +1,12 @@
 class PhotosController < ApplicationController
   def index
-    @photos = Photo.all
+    the_photos = Photo.all
+    @photos = the_photos.order({ :created_at => :desc })
     render({ :template => "photos/all_photos.html.erb"})
   end
 
   def create
-    user_id = params.fetch("input_owner_id")
+    user_id = session.fetch(:user_id)
     image = params.fetch("input_image")
     caption = params.fetch("input_caption")
     photo = Photo.new
@@ -19,6 +20,9 @@ class PhotosController < ApplicationController
   def show
     p_id = params.fetch("the_photo_id")
     @photo = Photo.where({:id => p_id }).first
+
+    @user_id = session.fetch(:user_id)
+
     render({:template => "photos/details.html.erb"})
   end
 
